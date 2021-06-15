b1d32cb1600222a9d9bc8676e850495924cbcebc
unknown <firra.andriani22@gmail.com>
2021-06-16 00:07:40 +0700

    Menangubah css catatan

--- app/views/catatan/edit.php
+++ app/views/catatan/edit.php
@@ -65,9 +65,6 @@
         <form action="<?= BASEURL; ?>/catatan/update/<?= $data['id_catatan']; ?>" method="post">
             <table class="form">
                 <tr>
-                    <td><h1>ID Catatan : <?= $data['id_catatan']; ?> </h1></td>
-                </tr>
-                <tr>
                     <td><label for="judulBuku">Judul Buku</label></td>
                     <td><input type="text" name="judul_buku" class="judul" value="<?= $data['judul_buku']; ?>"></td>
                 </tr>
